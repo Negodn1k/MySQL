74f0cc3a525678f7c74126b353727fb39a33675a
Илья <havkaset@gmail.com>
2023-09-08 12:06:02 +0300

    Do update and starter

--- src/main/java/org/example/SQL.java
+++ src/main/java/org/example/SQL.java
@@ -3,9 +3,27 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class SQL {
     List<Map<String, Object>> list = new ArrayList<>();
+
+    public SQL() {
+        start();
+    }
+
+    private void start() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter request");
+        String value = input.next();
+
+        switch (value.substring(0, 6).toLowerCase()) {
+            case "select" -> select();
+            case "update" -> update(value);
+            case "delete" -> delete();
+            case "insert" -> insert();
+        }
+    }
     private void insert() {
     }
 
@@ -15,11 +33,92 @@ public class SQL {
     private void delete() {
     }
 
-    private void update() {
+    private void update(String value) {
+        value = value.replaceAll("update", "");
+        value = value.replaceAll(" ", "");
+        value = value.replaceAll("'", "");
+        value = value.replaceAll("VALUES", "");
+        StringBuilder key = new StringBuilder();
+        StringBuilder mapValue = new StringBuilder();
+        StringBuilder oldKey = new StringBuilder();
+        StringBuilder oldValue = new StringBuilder();
+        StringBuilder comparison = new StringBuilder();
 
+        for (int i = 0; i < value.length(); i++) {
+            while (value.charAt(i) != '=') {
+                key.append(value.charAt(i));
+                if (key.toString().equals("WHERE")) {
+                    i++;
+                    key.delete(0, 1000);
+
+                    while (value.charAt(i) != '=' & value.charAt(i) != '<' & value.charAt(i) != '>') {
+                        oldKey.append(value.charAt(i));
+                        i++;
+                    }
+                    comparison.append(value.charAt(i));
+                    i++;
+                    while (value.charAt(i) != ',') {
+                        oldValue.append(value.charAt(i));
+                        i++;
+                    }
+                    break;
+                }
+                i++;
+            }
+            if (value.charAt(i) == '=') {
+                i++;
+                while (i < value.length()) {
+                    mapValue.append(value.charAt(i));
+                    i++;
+                }
+            }
+        }
+        if (comparison.toString().equals("=")) {
+            for (int i = 0; i < list.size(); i++) {
+                Map<String, Object> row = list.get(i);
+                try {
+                    if (row.containsKey(oldKey.toString()) & row.containsValue(Integer.parseInt(oldValue.toString()))) {
+                        row.replace(key.toString(), mapValue);
+                        list.set(i, row);
+                    }
+                } catch (NumberFormatException e) {
+                    if (row.containsKey(oldKey.toString()) & row.containsValue(oldValue.toString())) {
+                        row.replace(key.toString(), mapValue);
+                        list.set(i, row);
+                    }
+                }
+            }
+        }
+        if (comparison.toString().equals("<")) {
+            int oldIntValue = Integer.parseInt(oldValue.toString());
+            for (int i = 0; i < list.size(); i++) {
+                Map<String, Object> row = list.get(i);
+                for (int index = 0; index < oldIntValue; index++) {
+                    if (row.containsKey(oldKey.toString()) & row.containsValue(index)) {
+                        row.replace(key.toString(), mapValue);
+                        list.set(i, row);
+                    }
+                }
+            }
+        }
+        if (comparison.toString().equals(">")) {
+            int oldIntValue;
+            for (int i = 0; i < list.size(); i++) {
+                Map<String, Object> row = list.get(i);
+                oldIntValue = Integer.parseInt(oldValue.toString());
+                for (int index = oldIntValue; index < mapMaxValue(row); index++) {
+                    if (row.containsKey(oldKey.toString()) & row.get(oldKey.toString()).equals(oldIntValue)) {
+                        row.replace(key.toString(), mapValue);
+                        list.set(i, row);
+                        break;
+                    }
+                    oldIntValue++;
+                }
+            }
+        }
     }
 
-    private int mapMaxValue() {
+    private int mapMaxValue(Map<String, Object> map) {
         return 0;
     }
 }
