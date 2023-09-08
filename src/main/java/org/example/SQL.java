191b8ee7817a816baeab8d5a6ec525f792f53dca
Илья <havkaset@gmail.com>
2023-09-08 11:14:04 +0300

    Vinogradov - Max Value, select Tkachev - insert

--- src/main/java/org/example/SQL.java
+++ src/main/java/org/example/SQL.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class SQL {
+    List<Map<String, Object>> list = new ArrayList<>();
+    private void insert() {
+    }
+
+    private void select() {
+    }
+
+    private void delete() {
+    }
+
+    private void update() {
+
+    }
+
+    private int mapMaxValue() {
+        return 0;
+    }
 }
